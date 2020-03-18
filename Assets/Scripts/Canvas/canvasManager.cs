63acb385d0cb8230ba499d8c0eedff0faf90fdb9
sire <Simon.RE@etu.uca.fr>
2020-03-18 18:25:38 +0100

    Commit 1.0

---
+++ Assets/Scripts/Canvas/canvasManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class canvasManager : MonoBehaviour
+{
+    public GameObject accueil_canvas, collections_canvas, ma_collection_canvas;
+
+    int chosenCollectionId;
+
+    public Text textNomCollection;
+    public Text textNomCollectionBis;
+    public Text textDescription;
+
+    CollectionManager collectionManager;
+
+    void Start()
+    {
+        collectionManager = GameObject.Find("CollectionManager").GetComponent<CollectionManager>();
+    }
+
+    public void changeMyCanvas(int choice)
+    {
+        chosenCollectionId = choice;
+        switch (choice)
+        {
+            case 0:
+                break;
+            case 1:
+                textNomCollection.text = "Collection 1";
+                textNomCollectionBis.text = "Collection 1";
+                textDescription.text = "Vous pourrez profiter d'une collection raffinée inspirée de l'époque Victorienne.";
+                CollectionManager.chosenCollectionId = 1;
+                break;
+            case 2:
+                textNomCollection.text = "Collection 2";
+                textNomCollectionBis.text = "Collection 2";
+                textDescription.text = "Faites apparaître une collection simpliste et élégante.";
+                CollectionManager.chosenCollectionId = 2;
+                break;
+            case 3:
+                textNomCollection.text = "Collection 3";
+                textNomCollectionBis.text = "Collection 3";
+                textDescription.text = "Essayez une magnifique collection aux allures arrondies.";
+                CollectionManager.chosenCollectionId = 3;
+                break;
+            default:
+                Debug.LogError("Choix de collection non valide !");
+                break;
+        }
+    }
+
+    public void EnableAccueilCanvas()
+    {
+        accueil_canvas.SetActive(true);
+        collections_canvas.SetActive(false);
+        ma_collection_canvas.SetActive(false);
+    }
+    public void EnableCollectionsCanvas()
+    {
+        accueil_canvas.SetActive(false);
+        collections_canvas.SetActive(true);
+        ma_collection_canvas.SetActive(false);
+    }
+
+    public void EnableMaCollectionCanvas()
+    {
+        accueil_canvas.SetActive(false);
+        collections_canvas.SetActive(false);
+        ma_collection_canvas.SetActive(true);
+    }
+
+    public void clickValider()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void clickMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
+        Debug.Log("on quite l'application");
+    }
+}
---
+++ Assets/Scripts/CollectionManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectionManager : MonoBehaviour
+{
+    GameObject[] collection1;
+    GameObject[] collection2;
+    GameObject[] collection3;
+
+    public static int chosenCollectionId = 0;
+
+    bool modelsAreLoaded = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+        if (!modelsAreLoaded && SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(1))
+        {
+            modelsAreLoaded = true;
+            GetModels();
+            SetCollection();
+        }
+    }
+
+    void GetModels()
+    {
+        collection1 = GameObject.FindGameObjectsWithTag("Collection1");
+        collection2 = GameObject.FindGameObjectsWithTag("Collection2");
+        collection3 = GameObject.FindGameObjectsWithTag("Collection3");
+
+        foreach (GameObject go in collection1)
+        {
+            print(go.name);
+        }
+    }
+
+    void SetCollection()
+    {
+        //changement de la collection en fonction de la sélection
+        switch(chosenCollectionId)
+        {
+            case 1:
+                foreach(GameObject model in collection1) model.SetActive(true);
+                foreach(GameObject model in collection2) model.SetActive(false);
+                foreach(GameObject model in collection3) model.SetActive(false);
+                break;
+            case 2:
+                foreach(GameObject model in collection1) model.SetActive(false);
+                foreach(GameObject model in collection2) model.SetActive(true);
+                foreach(GameObject model in collection3) model.SetActive(false);
+                break;
+            case 3:
+                foreach(GameObject model in collection1) model.SetActive(false);
+                foreach(GameObject model in collection2) model.SetActive(false);
+                foreach(GameObject model in collection3) model.SetActive(true);
+                break;
+            default:
+                Debug.LogError("Collection non valide !");
+                break;
+        }
+    }
+}
